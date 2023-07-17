bf5639849936ff9ecc69a88d8d33c0873618e26f
StevenMODIMO <stevepetes26@gmail.com>
2023-07-17 09:26:15 +0300

    Initial Commit

--- client/src/components/Friends.jsx
+++ client/src/components/Friends.jsx
@@ -1,4 +1,15 @@
+import { useState, useEffect } from "react"
+import { useAuth } from "../context/AuthContext"
+
 export default function Friends() {
+    const [users, setUsers] = useState([])
+    const { user } = useAuth()
+
+    useEffect(() => {
+        const getAllUsers = async () => {
+            const response = await fetch(`http://localhost:5000/app/`)
+        }
+    })
     return (
         <div className="pt-10 mt-5">
             <h1 className="underline">Friends You Follow</h1>
